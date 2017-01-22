af98fbebfd683c1c41d115d0cee8eb8639e42bc0
michel crebassa <leonnoel95@hotmail.fr>
2017-01-22 10:52:00 +0100

    solution

---
+++ app.js
@@ -0,0 +1,35 @@
+(function () {
+'use strict';
+
+angular.module('MyAppli', [])
+.controller('LunchController', LunchController);
+
+//LunchController.$inject = ['$scope'];
+function LunchController($scope) {
+  $scope.lunchmenu = "" ;
+  $scope.message = "";
+//  console.log($scope.lunchmenu.toString());
+
+  $scope.traitementmenu = function () {
+//      console.log(angular.isArray(  $scope.lunchmenu));
+//      console.log( $scope.lunchmenu);
+      var stringsplit = $scope.lunchmenu.split(',');
+//      console.log(stringsplit.length);
+      if (stringsplit== "")
+      {
+        $scope.message="Please enter data first";
+      }
+      else if (stringsplit.length <4)
+       {
+          $scope.message="Enjoy!";
+      }
+      else
+       {
+          $scope.message="Too much!";
+      }
+//       console.log($scope.message) ;
+};
+
+}
+
+})();
