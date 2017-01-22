c0414c40e07b2a80c391bb1749fa6f974406c27a
michel crebassa <leonnoel95@hotmail.fr>
2017-01-22 14:42:52 +0100

    implementation css

--- app.js
+++ app.js
@@ -23,16 +23,39 @@ $scope.calculate = function () {
   if (stringsplit== "")
   {
     $scope.message="Please enter data first";
+    $scope.myObjText = {
+     "border" : "solid 1px red"
+    }
+    $scope.myObj = {
+   "color" : "red",
+    };
+
   }
   else if (stringsplit.length <4)
    {
       $scope.message="Enjoy!";
+      $scope.myObjText = {
+       "border" : "solid 1px green"
+      }
+      $scope.myObj = {
+     "color" : "green",
+      };
   }
   else
    {
       $scope.message="Too much!";
+      $scope.myObjText = {
+       "border" : "solid 1px green"
+      }
+
+      $scope.myObj = {
+      "color" : "green",
+      }
+
   }
        console.log($scope.message) ;
+       console.log($scope.myObj) ;
+
 };
 
 }
